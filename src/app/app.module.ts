5e978e2843a54e1720f49f33fa0adf4d17c29b48
Auronmatrix <auronmatrix@gmail.com>
2016-10-08 23:52:09 +0200

    Added Tick Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TickComponent } from './tick/tick.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TickComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tick/tick.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TickComponent } from './tick.component';
+
+describe('Component: Tick', () => {
+  it('should create an instance', () => {
+    let component = new TickComponent();
+    expect(component).toBeTruthy();
+  });
+});
