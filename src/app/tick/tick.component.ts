8f26499edf57df0fea700af4a31e2ef4838f33b6
Auronmatrix <auronmatrix@gmail.com>
2016-10-09 01:47:12 +0200

    Basic student

--- src/app/tick/tick.component.ts
+++ src/app/tick/tick.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tick.component.css']
 })
 export class TickComponent implements OnInit {
-
-  constructor() { }
+  title = "Class attendance checker";
+  students = []
+  constructor() {
+    this.students.push(new Student("John"));
+    this.students.push(new Student("Andrew"));
+    this.students.push(new Student("Jason"));
+  }
 
   ngOnInit() {
   }
 
+}
+
+class Student{
+  name : string;
+  lateCount : number;
+  absentCount : number;
+
+  constructor(studentName : string){
+    this.name = studentName;
+    this.lateCount = 0;
+    this.absentCount = 0;
+  }
 }
