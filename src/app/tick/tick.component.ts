84cf817c2f405e5a6749be0540b2839a3c3d0a26
Auronmatrix <auronmatrix@gmail.com>
2016-10-09 02:58:54 +0200

    Student Absent and Late Counters

--- src/app/tick/tick.component.ts
+++ src/app/tick/tick.component.ts
@@ -23,10 +23,79 @@ class Student{
   name : string;
   lateCount : number;
   absentCount : number;
+  status : string;
+  lateExpanded : boolean;
+  absentExpanded : boolean;
+  lateStamps = [];
+  absentStamps = [];
+
+
+  toggleLateExpanded()
+  {
+    this.lateExpanded = !(this.lateExpanded);
+  }
+
+  toggleAbsentExpanded()
+  {
+    this.absentExpanded = !this.absentExpanded;
+  }
+
+  setHere()
+  {
+    this.status = 'here';
+  }
+
+  setUnchecked()
+  {
+    this.status = 'unchecked';
+  }
+
+  disable()
+  {
+    return this.status != 'unchecked';
+  }
+
+  getDisabledClass()
+  {
+   return this.disable() ? 'disabled' : '';
+  }
+
+  setLate()
+  {
+    this.lateCount++;
+    this.status = 'late';
+    this.lateStamps.push(new Date())
+  }
+
+  setAbsent()
+  {
+    this.absentCount++;
+    this.status = 'absent';
+    this.absentStamps.push(new Date())
+  }
+
+  getStatusClass()
+  {
+    switch(this.status)
+    {
+      case 'unchecked':
+        return '';
+      case 'here':
+       return 'success';
+       case 'late':
+       return 'warning'
+       case 'absent':
+       return 'danger';
+    }
+    return '';
+  }
 
   constructor(studentName : string){
     this.name = studentName;
     this.lateCount = 0;
     this.absentCount = 0;
+    this.status = 'unchecked';
   }
+
+
 }
